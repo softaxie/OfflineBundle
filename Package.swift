14846bb8769a7eb35de6d2cb5fdd4d604aae3e02
asaxie <asaxie@metaarchit.com>
2026-06-12 14:50:31 +0800

    chore:修改包名

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "OjCapacitorOfflineBundle",
+    name: "CapacitorOfflineOfflineBundle",
     platforms: [.iOS(.v15)],
     products: [
         .library(
-            name: "OjCapacitorOfflineBundle",
+            name: "CapacitorOfflineOfflineBundle",
             targets: ["OfflineBundlePlugin"]
         )
     ],
